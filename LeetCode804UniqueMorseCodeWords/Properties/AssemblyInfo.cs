91f78af5518706f0910962da275295a9dd9c33e1
givemin5 <givemin5@gmail.com>
2019-02-07 01:03:01 +0800

    Add project files.

---
+++ LeetCode804UniqueMorseCodeWords/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCode804UniqueMorseCodeWords")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode804UniqueMorseCodeWords")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b47f74a-495e-4f0c-bb32-ad800440a0b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCode804UniqueMorseCodeWords/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode804UniqueMorseCodeWords
+{
+    public class Solution
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
